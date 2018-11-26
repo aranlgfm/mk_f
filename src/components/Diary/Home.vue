a562e6c456419e43f8ae127a44c81f78f02e1ce1
ellaHome <aranlgfm@gmail.com>
2018-11-27 00:18:43 +0900

    화면 수정함

--- src/components/Diary/Home.vue
+++ src/components/Diary/Home.vue
@@ -1,41 +1,63 @@
 <template lang="pug">
+div
     div
-        div
-            router-link(:to='{name:`Main`}') Logout
-            h1 여기가 실제 게시판
-            input(type=text)
-            button(@click='add') &#8629;
-        div
-            div date
-            div @{{name}}
-            div {{comment}}
-            button(@click='edit') 수정 또는 삭제 &#9998;
+        button(class='button is-primary btn' @click='goMain') Logout
+    hr
+    div(class='center')
+        b-field(grouped)
+            b-input(type='text' placeholder='comment' rounded expanded)
+            p(class="control")
+                button(@click='add' class='button is-primary btn') &#8629;
+    hr
+    div(class='center notification')
+        b-field(grouped)
+            div(class='commentRes') date
+            div(class='commentRes') @{{name}}
+            div(class='commentRes') {{comment}}
+            button(@click='edit' class='btn') 수정 또는 삭제 &#9998;
 </template>
 
 <script>
 export default {
-	name: 'home',
-	data() {
-		return {
-			name: 'aran',
-			comment: 'text test',
-		};
-	},
-	methods: {
-		add() {
-			console.log('등록을 할거임');
-		},
-		edit() {
-			console.log('수정 또는 삭제');
-		},
-	},
-	created() {},
+  // b-field(label="Name")
+  // 		b-input(value="Kevin Garvey")
+  name: 'home',
+  data() {
+    return {
+      name: 'aran',
+      comment: 'text test',
+    };
+  },
+  methods: {
+    add() {
+      console.log('등록을 할거임');
+    },
+    edit() {
+      console.log('수정 또는 삭제');
+    },
+    goMain() {
+      this.$router.push({ name: 'Main' })
+    }
+  },
+  created() { },
 };
 </script>
 
 <style scoped>
 #date {
-	font-family: 'Gaegu', cursive;
-	font-size: 1.5em;
+    font-family: 'Gaegu', cursive;
+    font-size: 1.5em;
+}
+.center {
+    margin: auto;
+    width: 500px;
+}
+
+.commentRes {
+    font-size: 1.2em;
+}
+
+.btn {
+    border-radius: 20px;
 }
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
     div
         h1 여기는 대문. 소셜로그인 붙일 것.
-        button(@click='login') login
+        button(@click='login' class='button is-primary') login
 </template>
 
 <script>
